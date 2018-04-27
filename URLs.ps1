55e2c945bce47e1c2de63ba94acfb08aceabcd63
PeterHorniak <peter.horniak.aust@gmail.com>
2018-04-28 09:43:43 +1000

    Downloading files from list of URLs
    These files show how you can provide PowerShell a list of URLs in a CSV file, and make it download each file and store it locally.

---
+++ URLs.ps1
@@ -0,0 +1,14 @@
+﻿# This script is for accessing every URL in a CSV file and saving the URL's file contents in a local folder. Each file will be named as per the filename in the URL.
+# Replace <filepath to CSV file> with where your file is stored, e.g. "C:\Users\Peter.Horniak\Desktop\URLs.csv".
+# Replace <filepath to folder outputs> with where you want the output files to be stored, e.g. "C:\Users\Peter.Horniak\Desktop\RylVideos\".
+# I have uploaded an example file called "URLs.csv". I created it using Power Query, which I uploaded as "URLs.xlsx".
+# It is possible to load the XLSX file directly into PowerShell, but this requires installing extra modules, so I just exported it into a CSV.
+
+$url = Get-Content "<filepath to CSV file>"
+
+Foreach ($i in $url)
+{
+ $name   = Split-Path $i -Leaf
+ $output = "<filepath to folder outputs>" + $name
+ Invoke-WebRequest -Uri $i -OutFile $output
+}
